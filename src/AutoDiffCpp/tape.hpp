1773ce185bb9a0dc712d61b5c70970bf1c4e3096
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-11 15:15:40 +0100

    works

--- src/AutoDiffCpp/tape.hpp
+++ src/AutoDiffCpp/tape.hpp
@@ -23,9 +23,10 @@ namespace AutoDiffCpp
     bool check_invariant() const
     {
       bool ok = true;
-      ok &= _offset_last_written < _offset_capacity;
-      ok &= _tape_size <= _tape_capacity;
-      ok &= _tape_size == _offset[_offset_last_written];
+      ok &= _offset_end <= _offset_capacity;
+      ok &= _tape_end <= _tape_capacity;
+      ok &= _offset_end > 0;
+      ok &= _tape_end == _offset[_offset_end - 1];
       return ok;
     }
 
@@ -53,26 +54,26 @@ namespace AutoDiffCpp
    public:
     Tape() : Tape(1024, 4 * 1024) {}
     Tape(const std::size_t offset_capacity, const std::size_t tape_capacity)
-        : _offset_last_written(0)
+        : _offset_end(1)
         , _offset_capacity(offset_capacity)
         , _offset(new offset_type[_offset_capacity])
-        , _tape_size(0)
+        , _tape_end(0)
         , _tape_capacity(tape_capacity)
         , _tape(new Index_PartialDiff<T>[tape_capacity])
     {
       assert(_offset_capacity > 0);
-      _offset[_offset_last_written] = 0;
+      _offset[0] = 0;
     }
 
-    std::size_t offset_capacity() const { return _offset_capacity; };
+    // std::size_t offset_capacity() const { return _offset_capacity; };
 
     void offset_resize(std::size_t new_size)
     {
       resize(new_size, _offset_capacity, _offset);
     }
 
-    std::size_t tape_size() const { return _tape_size; };
-    std::size_t tape_capacity() const { return _tape_capacity; };
+    // std::size_t tape_size() const { return _tape_end; };
+    // std::size_t tape_capacity() const { return _tape_capacity; };
 
     void tape_resize(std::size_t new_size)
     {
@@ -81,18 +82,19 @@ namespace AutoDiffCpp
 
     [[nodiscard]] Index_PartialDiff<T>* add_row(const std::size_t row_size)
     {
-      const auto row_offset_begin = _tape_size;
-      const auto row_offset_end = _tape_size + row_size;
+      assert(_offset[_offset_end - 1] == _tape_end);
 
-      offset_resize(_offset_last_written + 2);
+      const auto row_offset_begin = _tape_end;
+      _tape_end += row_size;
+      tape_resize(_tape_end);
 
-      assert(_offset[_offset_last_written] == row_offset_begin);
-      _offset[++_offset_last_written] = row_offset_end;
-
-      tape_resize(row_offset_end);
+      offset_resize(_offset_end + 1);
+      _offset[_offset_end++] = _tape_end;
 
       return _tape + row_offset_begin;
     }
+
+    std::size_t row_size() const { return _offset_end - 1; };
 
     ~Tape()
     {
@@ -101,14 +103,14 @@ namespace AutoDiffCpp
     }
 
    protected:
-    std::size_t _offset_last_written;  // = _offset_size - 1
+    std::size_t _offset_end;
     std::size_t _offset_capacity;
     offset_type* _offset;
 
     // NOTE: tape position cans deduced from offset last element (see
     // check_invariant()), however it is faster to have a direct
     // access
-    std::size_t _tape_size;
+    std::size_t _tape_end;
     std::size_t _tape_capacity;
     Index_PartialDiff<T>* _tape;
 
--- test/tape.cpp
+++ test/tape.cpp
@@ -7,9 +7,11 @@ TEST(Tape, demo)
 {
   Tape<double> tape;
 
-  ASSERT_EQ(tape.tape_size(), 0);
-  ASSERT_TRUE(tape.tape_capacity() > 0);
+  ASSERT_EQ(tape.row_size(), 0);
 
-  tape.add_row(1);
-  ASSERT_EQ(tape.tape_size(), 1);
+  auto* p = tape.add_row(1);
+  ASSERT_EQ(tape.row_size(), 1);
+
+  p = tape.add_row(10);
+  ASSERT_EQ(tape.row_size(), 2);
 }
