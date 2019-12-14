2baa818c06515508546e2d457472c81914e4c65a
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-14 13:42:16 +0100

    tape_vector

--- src/AutoDiffCpp/tape_vector.hpp
+++ src/AutoDiffCpp/tape_vector.hpp
@@ -4,4 +4,18 @@
 
 namespace AutoDiffCpp
 {
+  template <typename TAPE>
+  class Tape_Vector
+  {
+    friend TAPE;
+
+   public:
+    using index_type = typename TAPE::index_type;
+    using value_type = typename TAPE::value       cftrn,:§_type;
+
+   private:
+    const index_type _offset;
+    const index_type _size;
+    std::shared_array<
+  }
 }
