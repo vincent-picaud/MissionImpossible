18478b63736d250a6717a761ab1f72f3a356e9fd
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-14 19:09:19 +0100

    read/write mode for tape_vector

--- src/AutoDiffCpp/tape_vector.hpp
+++ src/AutoDiffCpp/tape_vector.hpp
@@ -9,6 +9,8 @@ namespace AutoDiffCpp
   template <typename TAPE>
   class Tape_Vector
   {
+    friend TAPE;  // gives write mode access to tape
+
    public:
     using index_type = typename TAPE::index_type;
     using value_type = typename TAPE::value_type;
@@ -18,6 +20,12 @@ namespace AutoDiffCpp
     const index_type _size;
     std::shared_ptr<value_type[]> _data;
 
+    value_type*
+    data()
+    {
+      return _data.get();
+    }
+
    public:
     Tape_Vector(const JamesBond_Tape<TAPE>& jb_tape)
         : _offset(jb_tape.index()), _size(jb_tape.size()), _data(new value_type[_size])
@@ -25,15 +33,22 @@ namespace AutoDiffCpp
       _data = std::shared_ptr<value_type[]>(_data, _data.get() - jb_tape.index());
     }
 
+    // all public methods are read only
     std::size_t
     size() const
     {
       return _size;
     };
-    value_type& operator[](const std::size_t i)
+
+    const value_type& operator[](const std::size_t i) const
     {
       assert((i > _offset) and (i - _offset < _size));
       return _data.get()[i];
+    }
+    const value_type*
+    data() const
+    {
+      return _data.get();
     }
   };
 }
