b5487c8433602f36ef982dad54bc38d6f396e4a8
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-16 19:35:29 +0100

    update op+

--- src/AutoDiffCpp/ad.hpp
+++ src/AutoDiffCpp/ad.hpp
@@ -351,27 +351,23 @@ namespace AutoDiffCpp
   // operator+ //
   ///////////////
   //
-  template <typename T>
-  AD_Expr<T, 1>
-  operator+(const Identity_t<T> g0, const AD<T>& g1)
-  {
-    using return_type         = decltype(g0 + g1);
-    using index_array_type    = typename return_type::index_array_type;
-    using partialD_array_type = typename return_type::partialD_array_type;
-
-    return return_type{g0 + g1.value(), index_array_type({g1.index()}), partialD_array_type({1})};
-  }
-  template <typename T>
+  template <typename T, typename IMPL1>
   auto
-  operator+(const AD<T>& g0, const Identity_t<T> g1)
+  operator+(const Identity_t<T> g0, const AD_Crtp<T, IMPL1>& g1)
   {
-    return g1 + g0;
+    return chain_rule(g0 + g1.value(), +1, g1);
+  }
+  template <typename T, typename IMPL1>
+  auto
+  operator+(const AD_Crtp<T, IMPL1>& g0, const Identity_t<T> g1)
+  {
+    return chain_rule(g0.value() + g1, +1, g0);
   }
   template <typename T, typename IMPL0, typename IMPL1>
   auto
   operator+(const AD_Crtp<T, IMPL0>& g0, const AD_Crtp<T, IMPL1>& g1)
   {
-    return chain_rule(g0.value() + g1.value(), 1, 1, g0, g1);
+    return chain_rule(g0.value() + g1.value(), 1, +1, g0, g1);
   }
 
   ///////////////
