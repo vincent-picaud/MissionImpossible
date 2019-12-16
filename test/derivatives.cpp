bc6945bd3dbf3fd044f20a3afdfedce197b7ff5b
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-16 18:36:37 +0100

    addition and substraction first tests

--- test/derivatives.cpp
+++ test/derivatives.cpp
@@ -24,3 +24,61 @@ TEST(Derivatives, product)
   EXPECT_EQ(grad[x1], 6);
   EXPECT_EQ(grad[y], 1);
 }
+
+TEST(Derivatives, substraction)
+{
+  AD<double> x0(3), x1(4), y;
+
+  y = 2 - x0;
+
+  auto grad = gradient(y);
+  EXPECT_EQ(y.value(), -1);
+  EXPECT_EQ(grad[x0], -1);
+  EXPECT_EQ(grad[x1], 0);
+  EXPECT_EQ(grad[y], 1);
+
+  y = 2 * x0 - x1;
+
+  grad = gradient(y);
+  EXPECT_EQ(y.value(), 2);
+  EXPECT_EQ(grad[x0], 2);
+  EXPECT_EQ(grad[x1], -1);
+  EXPECT_EQ(grad[y], 1);
+
+  y = 2 * x0 - 4 * x1;
+
+  grad = gradient(y);
+  EXPECT_EQ(y.value(), -10);
+  EXPECT_EQ(grad[x0], 2);
+  EXPECT_EQ(grad[x1], -4);
+  EXPECT_EQ(grad[y], 1);
+}
+
+TEST(Derivatives, addition)
+{
+  AD<double> x0(3), x1(4), y;
+
+  y = 2 + x0;
+
+  auto grad = gradient(y);
+  EXPECT_EQ(y.value(), 5);
+  EXPECT_EQ(grad[x0], +1);
+  EXPECT_EQ(grad[x1], 0);
+  EXPECT_EQ(grad[y], 1);
+
+  y = 2 * x0 + x1;
+
+  grad = gradient(y);
+  EXPECT_EQ(y.value(), 10);
+  EXPECT_EQ(grad[x0], 2);
+  EXPECT_EQ(grad[x1], +1);
+  EXPECT_EQ(grad[y], 1);
+
+  y = 2 * x0 + 4 * x1;
+
+  grad = gradient(y);
+  EXPECT_EQ(y.value(), 22);
+  EXPECT_EQ(grad[x0], 2);
+  EXPECT_EQ(grad[x1], 4);
+  EXPECT_EQ(grad[y], 1);
+}
