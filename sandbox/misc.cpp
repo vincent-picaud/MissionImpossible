d3cecb3f9626abbc74f4caef3aa44bcb21769119
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-18 15:26:50 +0100

    misc.cpp: contains interesting example
    -> showing tape filling during nested mode -> computing the gradient
    increase AD<> tape

--- sandbox/misc.cpp
+++ sandbox/misc.cpp
@@ -1,9 +1,6 @@
-// Investigate why
+// This show that in case of nested type, y gradient computation
+// add element to the tape
 //
-// AD<AD<double>> x0(3);
-// x0 == 3;
-//
-// create an instance
 #include "AutoDiffCpp/ad.hpp"
 #include "AutoDiffCpp/derivatives.hpp"
 
@@ -19,8 +16,24 @@ main()
   std::cout << "Tape memory " << x0.tape().memory_size() << "\t" << x0.value().tape().memory_size()
             << std::endl;
 
-  y = x0 + x1;
+  y = x0 + 2 * x1;
 
   std::cout << "Tape memory " << x0.tape().memory_size() << "\t" << x0.value().tape().memory_size()
             << std::endl;
+
+  std::cout << "Tape AD<AD>\n" << x0.tape() << std::endl;
+  std::cout << "Tape AD<T>\n" << x0.value().tape() << std::endl;
+
+  // NOTE: this increase the AD<T> tape
+  //       -> this is normal, as we also compute second order derivatives
+  //
+  auto grad_y = gradient(y);
+
+  std::cout << "grad_y\n" << grad_y;
+
+  std::cout << "Tape memory " << x0.tape().memory_size() << "\t" << x0.value().tape().memory_size()
+            << std::endl;
+
+  std::cout << "Tape AD<AD>\n" << x0.tape() << std::endl;
+  std::cout << "Tape AD<T>\n" << x0.value().tape() << std::endl;
 }
