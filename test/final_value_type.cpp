a8e0101822ef1de8c2e92bc726e3ea00d8fec108
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-18 10:01:46 +0100

    add Final_Value_Type tests

---
+++ test/final_value_type.cpp
@@ -0,0 +1,25 @@
+#include "AutoDiffCpp/final_value_type.hpp"
+#include "AutoDiffCpp/ad.hpp"  // CAVEAT: must be included otherwise
+                               // tests fail (incomplete type)
+
+#include <gtest/gtest.h>
+
+using namespace AutoDiffCpp;
+
+TEST(Final_Value_Type, basic)
+{
+  using T = double;
+
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<T>>));
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD<T>>>));
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD_Expr<T, 4>>>));
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD_Crtp<T, AD<T>>>>));
+
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD<AD<T>>>>));
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD_Expr<AD<T>, 4>>>));
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD_Crtp<T, AD<AD<T>>>>>));
+
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD<AD<AD<T>>>>>));
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD_Expr<AD<AD<T>>, 4>>>));
+  ASSERT_TRUE((std::is_same_v<T, AD_Final_Value_Type_t<AD_Crtp<T, AD<AD<AD<T>>>>>>));
+}
