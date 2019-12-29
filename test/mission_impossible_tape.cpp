4d877a4a7984539f7d0a100da1a93cdd3ed96de6
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-29 11:37:43 +0100

    mission_impossible_tape first tests

--- test/mission_impossible_tape.cpp
+++ test/mission_impossible_tape.cpp
@@ -1,4 +1,5 @@
 #include "AutoDiffCpp/mission_impossible_tape.hpp"
+#include "AutoDiffCpp/ad.hpp"
 
 #include <gtest/gtest.h>
 
@@ -7,6 +8,12 @@ using namespace AutoDiffCpp;
 TEST(Mission_Impossible_Tape, construction)
 {
   Mission_Impossible_Tape<double> mark;
+
+  EXPECT_EQ(mark.size(), 0);
+}
+TEST(Mission_Impossible_Tape, construction_nested)
+{
+  Mission_Impossible_Tape<AD<AD<double>>> mark;
 
   EXPECT_EQ(mark.size(), 0);
 }
