b45a355730727011fd6bd74b1f17fe2efcf762ea
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-29 11:49:12 +0100

    add fail test
    -> for the moment gradient does not use impossible_mission_tape

--- test/mission_impossible_tape.cpp
+++ test/mission_impossible_tape.cpp
@@ -1,5 +1,6 @@
 #include "AutoDiffCpp/mission_impossible_tape.hpp"
 #include "AutoDiffCpp/ad.hpp"
+#include "AutoDiffCpp/derivatives.hpp"
 
 #include <gtest/gtest.h>
 
@@ -11,9 +12,37 @@ TEST(Mission_Impossible_Tape, construction)
 
   EXPECT_EQ(mark.size(), 0);
 }
+
 TEST(Mission_Impossible_Tape, construction_nested)
 {
   Mission_Impossible_Tape<AD<AD<double>>> mark;
 
   EXPECT_EQ(mark.size(), 0);
+}
+
+TEST(Mission_Impossible_Tape, local)
+{
+  using T = double;
+
+  AD<T> x1(1), x2(2), y;
+
+  y = 2 * x1 * x2;
+
+  auto pos     = tape<T>().row_size();
+  auto pos_mem = tape<T>().memory_size();
+
+  {
+    Mission_Impossible_Tape<T> self_destructing_tape;
+
+    AD<T> a(1), b(2), c;
+
+    c = 2 * a * b;
+
+    auto grad = gradient(c);
+
+    EXPECT_EQ(grad.size(), 3);
+  }
+
+  EXPECT_EQ(pos, tape<T>().row_size());
+  EXPECT_EQ(pos_mem, tape<T>().memory_size());
 }
