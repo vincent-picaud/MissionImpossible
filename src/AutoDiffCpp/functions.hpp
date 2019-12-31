2a8b032dba3f54b5144e60d1078161fe4ba0f692
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-31 10:30:28 +0100

    add sin

--- src/AutoDiffCpp/functions.hpp
+++ src/AutoDiffCpp/functions.hpp
@@ -15,4 +15,13 @@ namespace AutoDiffCpp
     const auto v = exp(x.value());
     return AD_Function(v, v * x.differential());
   }
+
+  template <typename T, typename IMPL>
+  inline auto
+  sin(const AD_Crtp<T, IMPL>& x) noexcept
+  {
+    using std::sin;
+
+    return AD_Function(sin(x.value()), cos(x.value()) * x.differential());
+  }
 }
--- test/functions.cpp
+++ test/functions.cpp
@@ -14,3 +14,12 @@ TEST(Function, exp)
   ASSERT_DOUBLE_EQ(y.value(), exp(x.value()));
   ASSERT_DOUBLE_EQ(grad[x], exp(x.value()));
 }
+
+TEST(Function, sin)
+{
+  AD<double> x = 2, y;
+  y            = sin(x);
+  auto grad    = gradient(y);
+  ASSERT_DOUBLE_EQ(y.value(), sin(x.value()));
+  ASSERT_DOUBLE_EQ(grad[x], cos(x.value()));
+}
