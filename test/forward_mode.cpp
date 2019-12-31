8351d01f6f03bd4d7bef8bc569c1a941a370fe6a
Picaud Vincent <picaud.vincent@gmail.com>
2019-12-31 11:19:32 +0100

    add forward mode test

---
+++ test/forward_mode.cpp
@@ -0,0 +1,25 @@
+#include "AutoDiffCpp/derivatives.hpp"
+#include "AutoDiffCpp/functions.hpp"
+
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace AutoDiffCpp;
+
+TEST(Forward_mode, basic)
+{
+  AD<double> x1 = 2, x2 = 3, y1, y2;
+
+  y1 = cos(x1 * x2);
+  y2 = sin(x1 * x2);
+
+  auto J_column_1 = Jacobian_column(x1);
+
+  ASSERT_DOUBLE_EQ(J_column_1[y1], -x2.value() * sin(x1.value() * x2.value()));
+  ASSERT_DOUBLE_EQ(J_column_1[y2], x2.value() * cos(x1.value() * x2.value()));
+
+  auto J_column_2 = Jacobian_column(x2);
+
+  ASSERT_DOUBLE_EQ(J_column_2[y1], -x1.value() * sin(x1.value() * x2.value()));
+  ASSERT_DOUBLE_EQ(J_column_2[y2], x1.value() * cos(x1.value() * x2.value()));
+}
