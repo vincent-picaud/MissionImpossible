787bcb0864e6ab19cf678c3ce6d4bd653f36b566
Picaud Vincent <picaud.vincent@gmail.com>
2020-01-03 12:39:24 +0100

    add Hessian action H.v example

--- sandbox/nested_2nd_order.cpp
+++ sandbox/nested_2nd_order.cpp
@@ -5,9 +5,50 @@
 
 using namespace AutoDiffCpp;
 
+template <typename T>
+auto
+Rosenbrock(const T& x0, const T& x1)
+{
+  return (1 - x0) * (1 - x0) + 10 * (x1 - x0 * x0) * (x1 - x0 * x0);
+}
+
+void
+directional()
+{
+  AD<AD<double>> x0(3), x1(4), y;
+
+  y = Rosenbrock(x0, x1);
+
+  assert(y == 254);
+
+  auto y_gradient = Jacobian_row(y);
+
+  assert(y_gradient[x0] == 604);
+  assert(y_gradient[x1] == -100);
+
+  AD<double> v0(5), v1(6), z;
+  z = v0 * y_gradient[x0] + v1 * y_gradient[x1];
+
+  auto z_gradient = Jacobian_row(z);
+
+  // auto Hessian_x0_column = Jacobian_column(y_gradient[x1]);
+
+  // // assert(Hessian_x0_column[x0] == 922);
+  // // assert(Hessian_x0_column[x1] == -120);
+
+  std::cerr << z_gradient[x0] << std::endl;
+  std::cerr << z_gradient[x1] << std::endl;
+
+  std::cout << "Tape<T>\n" << x0.value().tape();
+  std::cout << "Tape<AD<T>>\n" << x0.tape();
+}
+
 int
 main()
 {
+  directional();
+  return 0;
+
   AD<AD<double>> x0(3), x1(4), y;
 
   y = (1 - x0) * (1 - x0) + 10 * (x1 - x0 * x0) * (x1 - x0 * x0);
---
+++ test/Hv.cpp
@@ -0,0 +1,30 @@
+#include "AutoDiffCpp/ad.hpp"
+#include "AutoDiffCpp/derivatives.hpp"
+
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace AutoDiffCpp;
+
+// Idea H.v = ∇_w(v.∇_w f)
+// See: http://www.bcl.hamilton.ie/~barak/papers/nc-hessian.pdf
+TEST(Nested, Hv)
+{
+  AD<AD<double>> x0(3), x1(4), y;
+  y = (1 - x0) * (1 - x0) + 10 * (x1 - x0 * x0) * (x1 - x0 * x0);
+
+  assert(y == 254);
+
+  auto y_gradient = Jacobian_row(y);  // Compute ∇_w f
+
+  assert(y_gradient[x0] == 604);
+  assert(y_gradient[x1] == -100);
+
+  AD<double> v0(5), v1(6), z;
+  z = v0 * y_gradient[x0] + v1 * y_gradient[x1];  // Compute v.∇_w f
+
+  auto z_gradient = Jacobian_row(z);  // Compute ∇_w(v.∇_w f) = H.v
+
+  assert(z_gradient[x0] == 3890);
+  assert(z_gradient[x1] == -480);
+}
