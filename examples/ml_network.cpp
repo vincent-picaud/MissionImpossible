1487af369ee59d93d2795120e1928f463583cf1f
Picaud Vincent <picaud.vincent@gmail.com>
2020-01-03 16:53:01 +0100

    add network example

--- examples/ml_network.cpp
+++ examples/ml_network.cpp
@@ -24,6 +24,13 @@ std::normal_distribution<> d{0, 1};
 template <typename T, std::size_t N>
 struct TinyVector : public std::array<T, N>
 {
+  TinyVector
+  operator=(const std::array<T, N>& v)
+  {
+    (*this) = v;
+    return *this;
+  }
+
   friend std::ostream&
   operator<<(std::ostream& out, const TinyVector& tv)
   {
@@ -36,7 +43,7 @@ struct TinyVector : public std::array<T, N>
 };
 
 template <typename T, std::size_t N, std::size_t M>
-struct TinyMatrix : std::array<TinyVector<T, N>, M>
+struct TinyMatrix : std::array<TinyVector<T, M>, N>
 {
   friend std::ostream&
   operator<<(std::ostream& out, const TinyMatrix& tv)
@@ -58,6 +65,7 @@ initialize(TinyVector<T, N>& a)
     a_i = 0.5 * d(gen);
   }
 }
+
 template <typename T, std::size_t N, std::size_t M>
 void
 initialize(TinyMatrix<T, N, M>& a)
@@ -68,14 +76,122 @@ initialize(TinyMatrix<T, N, M>& a)
   }
 }
 
+template <typename T, std::size_t N>
+T
+dot(const TinyVector<T, N>& v, const TinyVector<T, N>& w)
+{
+  T sum = 0;
+
+  for (size_t i = 0; i < N; i++)
+  {
+    sum += v[i] * w[i];
+  }
+  return sum;
+}
+
+template <typename T, std::size_t N, std::size_t M>
+TinyVector<T, N> operator*(const TinyMatrix<T, N, M>& A, const TinyVector<T, M>& v)
+{
+  TinyVector<T, N> w;
+  for (size_t i = 0; i < N; i++)
+  {
+    w[i] = dot(A[i], v);
+  }
+  return w;
+}
+template <typename T, std::size_t N>
+TinyVector<T, N>
+operator+(const TinyVector<T, N>& u, const TinyVector<T, N>& v)
+{
+  TinyVector<T, N> w;
+
+  for (size_t i = 0; i < N; i++)
+  {
+    w[i] = u[i] + v[i];
+  }
+  return w;
+}
+
+template <typename T>
+T
+sigmoid(const T& x)
+{
+  return 1 / (1 + exp(-x));
+}
+
+template <typename T, std::size_t N>
+TinyVector<T, N>
+sigmoid(const TinyVector<T, N>& v)
+{
+  TinyVector<T, N> w;
+  for (size_t i = 0; i < N; i++)
+  {
+    w[i] = sigmoid(v[i]);
+  }
+  return w;
+}
+
+template <typename T, std::size_t N, std::size_t M>
+TinyVector<T, N>
+layer(const TinyMatrix<T, N, M>& W, const TinyVector<T, N>& b, const TinyVector<T, M>& x)
+{
+  return sigmoid(W * x + b);
+}
+
+// template <typename T>
+// T
+// network(const T& x)
+
+template <typename T, std::size_t N, std::size_t M, typename CLASSIFIER>
+T
+cost_quadratic(const TinyMatrix<T, N, M>& X,
+               const TinyVector<T, N>& y,
+               const CLASSIFIER& classifier)
+{
+  T sum = 0;
+  for (size_t i = 0; i < N; i++)
+  {
+    sum += pow(y[i] - classifier(X[i]), 2);
+  }
+
+  return sum / N;
+}
+
+//////////////////////////////////////////////////////////////////
+
+auto
+data_X()
+{
+  TinyMatrix<double, 10, 2> X;
+  X[0] = std::array{0.1, 0.1};
+  X[1] = std::array{0.3, 0.4};
+  X[2] = std::array{0.1, 0.5};
+  X[3] = std::array{0.6, 0.9};
+  X[4] = std::array{0.4, 0.2};
+  X[5] = std::array{0.6, 0.3};
+  X[6] = std::array{0.5, 0.6};
+  X[7] = std::array{0.9, 0.2};
+  X[8] = std::array{0.4, 0.4};
+  X[9] = std::array{0.7, 0.6};
+
+  // std::array{1, 2};
+  return X;
+}
 int
 main()
 {
-  using T = double;
+  //  using T = double;
+  using T = AD<double>;
 
-  TinyMatrix<AD<T>, 2, 3> V;
+  TinyVector<T, 3> x;
+  TinyVector<T, 2> b;
+  TinyMatrix<T, 2, 3> M;
 
-  initialize(V);
+  initialize(x);
+  initialize(b);
+  initialize(M);
 
-  std::cout << V;
+  auto X = data_X();
+  std::cout << layer(M, b, x);
+  std::cout << X;
 }
