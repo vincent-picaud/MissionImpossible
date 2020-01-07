513ef297a21a1c8924806bd936a4aef3de91b878
Picaud Vincent <picaud.vincent@gmail.com>
2020-01-07 11:19:55 +0100

    doc

--- examples/Jacobian.cpp
+++ examples/Jacobian.cpp
@@ -7,10 +7,7 @@ using namespace MissionImpossible;
 int
 main()
 {
-  AD<double> r, theta;
-
-  r     = 2;
-  theta = 0.1;
+  AD<double> r = 2, theta = 0.1;
 
   AD<double> y1 = r * cos(theta);
   AD<double> y2 = r * sin(theta);
@@ -21,26 +18,17 @@ main()
   //
   // -> AKA reverse-mode
   //
-  std::cout << std::endl << "Jacobian row by row" << std::endl;
+  std::cout << "Jacobian row by row" << std::endl;
 
-  // y1 row (which is also ∇y1)
-  // -> you can also replace "Jacobian_row(y1)" by "gradient(y1)"
-  //
-  auto Jacobian_row_y1 = Jacobian_row(y1);
+  auto Jacobian_row_y1 = Jacobian_row(y1);  // ∇y1
 
-  std::cout << "∇y1[r] = " << std::setw(20) << Jacobian_row_y1[r]
-            << " (must be cos(θ) = " << cos(theta) << ")" << std::endl;
-  std::cout << "∇y1[θ] = " << std::setw(20) << Jacobian_row_y1[theta]
-            << " (must be -r*sin(θ) = " << -r * sin(theta) << ")" << std::endl;
+  std::cout << "∇y1(r,θ) = " << std::setw(20) << Jacobian_row_y1[r] << ", " << std::setw(20)
+            << Jacobian_row_y1[theta] << std::endl;
 
-  // In the same way, one can compute ∇y2
-  //
-  auto Jacobian_row_y2 = Jacobian_row(y2);
+  auto Jacobian_row_y2 = Jacobian_row(y2);  // ∇y2
 
-  std::cout << "∇y2[r] = " << std::setw(20) << Jacobian_row_y2[r]
-            << " (must be sin(θ) = " << sin(theta) << ")" << std::endl;
-  std::cout << "∇y2[θ] = " << std::setw(20) << Jacobian_row_y2[theta]
-            << " (must be +r*cos(θ) = " << r * cos(theta) << ")" << std::endl;
+  std::cout << "∇y2(r,θ) = " << std::setw(20) << Jacobian_row_y2[r] << ", " << std::setw(20)
+            << Jacobian_row_y2[theta] << std::endl;
 
   ////////////////////////////////////////
   // Computes Jacobian column by column //
@@ -50,21 +38,13 @@ main()
   //
   std::cout << std::endl << "Jacobian column by column" << std::endl;
 
-  // r column
-  //
-  auto Jacobian_column_r = Jacobian_column(r);
+  auto Jacobian_column_r = Jacobian_column(r);  // r column
 
-  std::cout << "∂r y1  = " << std::setw(20) << Jacobian_column_r[y1]
-            << " (must be cos(θ) = " << cos(theta) << ")" << std::endl;
-  std::cout << "∂r y2  = " << std::setw(20) << Jacobian_column_r[y2]
-            << " (must be sin(θ) = " << sin(theta) << ")" << std::endl;
+  std::cout << "∂r y1  = " << std::setw(20) << Jacobian_column_r[y1] << std::endl;
+  std::cout << "∂r y2  = " << std::setw(20) << Jacobian_column_r[y2] << std::endl;
 
-  // θ column
-  //
-  auto Jacobian_column_theta = Jacobian_column(theta);
+  auto Jacobian_column_theta = Jacobian_column(theta);  // θ column
 
-  std::cout << "∂θ y1  = " << std::setw(20) << Jacobian_column_theta[y1]
-            << " (must be -r*sin(θ) = " << -r * sin(theta) << ")" << std::endl;
-  std::cout << "∂θ y2  = " << std::setw(20) << Jacobian_column_theta[y2]
-            << " (must be +r*cos(θ) = " << r * cos(theta) << ")" << std::endl;
+  std::cout << "∂θ y1  = " << std::setw(20) << Jacobian_column_theta[y1] << std::endl;
+  std::cout << "∂θ y2  = " << std::setw(20) << Jacobian_column_theta[y2] << std::endl;
 }
