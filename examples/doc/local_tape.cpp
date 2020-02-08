4481c34f19d8a09759ef4614345c96c55466c669
Picaud Vincent <picaud.vincent@gmail.com>
2020-02-08 09:11:59 +0100

    local tape example (to finish)

---
+++ examples/doc/local_tape.cpp
@@ -0,0 +1,48 @@
+#include "MissionImpossible/MissionImpossible.hpp"
+
+using namespace MissionImpossible;
+
+int
+main()
+{
+  auto print_tape_size = []() { std::cout << "size: " << tape<double>().row_size() << std::endl; };
+
+
+  // GOOD
+  //================
+  print_tape_size();
+
+  {
+    MissionImpossible_Tape<double> local_tape;
+
+    AD<double> x0 = 2, x1 = 3, y;
+
+    y = 4 * x0 + 2 * x1;
+
+    auto grad = gradient(local_tape, y);
+    std::cout << y << ", " << grad[x0] << ", " << grad[x1] << std::endl;
+    print_tape_size();
+  }
+
+  print_tape_size();
+
+  // GOOD
+  //================
+  AD<double> a=1,b=2; // Ok, as a, b are not used in local_tape scope 
+
+  print_tape_size();  
+  {
+    MissionImpossible_Tape<double> local_tape;
+
+    AD<double> x0 = 2, x1 = 3, y;
+
+    y = 4 * x0 + 2 * x1;
+
+    auto grad = gradient(local_tape, y);
+    std::cout << y << ", " << grad[x0] << ", " << grad[x1] << std::endl;
+    print_tape_size();
+  }
+
+  print_tape_size();
+ 
+}
